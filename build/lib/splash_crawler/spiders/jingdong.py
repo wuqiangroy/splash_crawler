0d98ee8df045308e9fc0b20085925e5e509ab84d
wuqiangroy <wuqiangroy@live.com>
2018-04-23 23:58:29 +0800

    update

---
+++ build/lib/splash_crawler/spiders/jingdong.py
@@ -0,0 +1,24 @@
+"""
+这是一个示例，用来爬取 ajax 的京东商品信息
+"""
+
+from scrapy import Spider, Request, Selector
+
+
+class JingDongSpider(Spider):
+
+    name = "jingdong_phone_price"
+
+    def start_requests(self):
+        url = "https://list.jd.com/list.html?cat=9987,653,655"
+
+        yield Request("http://localhost:8050/render.html?url={}".format(url), callback=self.parse)
+
+    def parse(self, response):
+        data = []
+        res = response.xpath("//li[@class='gl-item']").extract()
+        for i in res:
+            name = Selector(text=str(i)).xpath("//div[@class='p-name']//em/text()").extract_first()
+            price = Selector(text=str(i)).xpath("//strong[@class='J_price']//i/text()").extract_first()
+            data.append({name.strip("\n\t "): price})
+        return data
